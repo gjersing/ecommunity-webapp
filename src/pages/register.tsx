0aa0861d7e914fb0bd15303e47fd91d5fd8440ec
gjersing <csgjersing@gmail.com>
2024-01-30 18:57:27 -0800

    Setup urql, add graphql-codegen, and generate schema for Register

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -10,19 +10,19 @@ import {
 } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import InputField from "../components/InputField";
+import { useMutation } from "urql";
+import { useRegisterMutation } from "../graphql/generated/graphql";
 
 interface registerProps {}
 
 export const Register: React.FC<registerProps> = ({}) => {
+  const [, register] = useRegisterMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values, actions) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            actions.setSubmitting(false);
-          }, 1000);
+        onSubmit={async (values, actions) => {
+          const response = await register(values);
         }}
       >
         {(props) => (
