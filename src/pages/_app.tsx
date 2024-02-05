b4a833d2efe1f20ab4fd26bc604fe11cf60f76c2
gjersing <csgjersing@gmail.com>
2024-02-05 12:22:01 -0800

    type updateQuery & set current_user on login/register

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,71 @@
 import { ChakraProvider } from "@chakra-ui/react";
-
 import theme from "../theme";
 import { AppProps } from "next/app";
-import { Client, Provider, cacheExchange, fetchExchange } from "urql";
+import { Client, Provider, fetchExchange } from "urql";
+import { Cache, QueryInput, cacheExchange } from "@urql/exchange-graphcache";
+import {
+  CurrentUserDocument,
+  CurrentUserQuery,
+  LoginMutation,
+  RegisterMutation,
+} from "../graphql/generated/graphql";
+
+function typedUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = new Client({
   url: "http://localhost:4000/graphql",
-  exchanges: [cacheExchange, fetchExchange],
+  exchanges: [
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            typedUpdateQuery<LoginMutation, CurrentUserQuery>(
+              cache,
+              {
+                query: CurrentUserDocument,
+              },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    current_user: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          register: (_result, args, cache, info) => {
+            typedUpdateQuery<RegisterMutation, CurrentUserQuery>(
+              cache,
+              {
+                query: CurrentUserDocument,
+              },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    current_user: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
   fetchOptions: {
     credentials: "include",
   },
