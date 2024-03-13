b43636769aec3beff394671000522abcbbf1de91
gjersing <csgjersing@gmail.com>
2024-03-13 12:24:15 -0700

    Forward users to login if they visit create-post unauthenticated

--- src/pages/create-post.tsx
+++ src/pages/create-post.tsx
@@ -1,16 +1,26 @@
 import { Button, Flex } from "@chakra-ui/react";
 import { Formik, Form } from "formik";
 import router from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
 import Wrapper from "../components/Wrapper";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { NavBar } from "../components/NavBar";
 import TextAreaField from "../components/TextAreaField";
-import { useCreatePostMutation } from "../graphql/generated/graphql";
+import {
+  useCreatePostMutation,
+  useCurrentUserQuery,
+} from "../graphql/generated/graphql";
 
 const CreatePost: React.FC = ({}) => {
+  const [{ data, fetching }] = useCurrentUserQuery();
   const [, createPost] = useCreatePostMutation();
+
+  useEffect(() => {
+    if (!data?.current_user && !fetching) {
+      router.replace("/login");
+    }
+  }, [data, router]);
 
   return (
     <div className="createPost-container">
@@ -20,8 +30,10 @@ const CreatePost: React.FC = ({}) => {
           initialValues={{ body: "" }}
           onSubmit={async (values) => {
             // TO DO: Validate user inputs against obscenity filter
-            await createPost({ input: values });
-            router.push("/");
+            const { error } = await createPost({ input: values });
+            if (!error) {
+              router.push("/");
+            }
           }}
         >
           {(props) => (
