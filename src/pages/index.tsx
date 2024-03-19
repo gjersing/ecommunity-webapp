d1af96da8f02f0b10567f179aeaebacf4b6fc940
gjersing <csgjersing@gmail.com>
2024-03-19 15:13:10 -0700

    Remove load more button and reduce post pagination limit to 10

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,13 @@ import { NavBar } from "../components/NavBar";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../graphql/generated/graphql";
-import { VStack, Spinner, Button, Flex } from "@chakra-ui/react";
+import { VStack, Spinner } from "@chakra-ui/react";
 import { PostCard } from "../components/PostCard";
 import { useEffect, useState } from "react";
 
 const Index = () => {
   const [variables, setVariables] = useState({
-    limit: 20,
+    limit: 10,
     cursor: null as null | string,
   });
   const [{ data, fetching }] = usePostsQuery({
@@ -53,24 +53,8 @@ const Index = () => {
         ) : (
           data.posts.posts.map((p) => <PostCard post={p} key={p.id} />)
         )}
+        {fetching && data?.posts.hasMore ? <Spinner /> : null}
       </VStack>
-      {data && data.posts.hasMore ? (
-        <Flex>
-          <Button
-            onClick={() => {
-              setVariables({
-                limit: variables.limit,
-                cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
-              });
-            }}
-            isLoading={fetching}
-            m="auto"
-            mb={8}
-          >
-            Load More
-          </Button>
-        </Flex>
-      ) : null}
     </Container>
   );
 };
