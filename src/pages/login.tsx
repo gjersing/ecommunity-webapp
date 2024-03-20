32ef439e7338d0b2fbd5f9956aa19c93fbebbd1a
gjersing <csgjersing@gmail.com>
2024-03-20 12:12:28 -0700

    style login & register with card - add sign up/sign in link

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { Form, Formik } from "formik";
-import { Button, Box, Link, Flex } from "@chakra-ui/react";
+import {
+  Button,
+  Box,
+  Link,
+  Flex,
+  Center,
+  Heading,
+  Text,
+  Divider,
+  Card,
+} from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import InputField from "../components/InputField";
 import { useLoginMutation } from "../graphql/generated/graphql";
@@ -38,41 +48,64 @@ const Login: React.FC<loginProps> = ({}) => {
           }}
         >
           {(props) => (
-            <Form>
-              <InputField
-                name="usernameOrEmail"
-                placeholder="Email or username"
-                label="Email or username"
-              />
-              <Box mt={4}>
-                <InputField
-                  name="password"
-                  placeholder="password"
-                  label="Password"
-                  type="Password"
-                />
-                <Flex>
+            <Card
+              p={8}
+              boxShadow="rgba(0, 0, 0, 0.12) 0px 2px 8px 0px, rgba(0, 0, 0, 0.16) 0px 0px 2px 0px"
+            >
+              <Form>
+                <Center>
+                  <Heading>Sign in</Heading>
+                </Center>
+                <Flex my={2} justifyContent="center">
+                  <Text fontSize={"0.75rem"} color="gray">
+                    DON'T HAVE AN ACCOUNT?
+                  </Text>
                   <Link
-                    href="/forgot-password"
+                    href="/register"
                     as={NextLink}
                     color="blue"
                     fontSize={"0.75rem"}
-                    ml={"auto"}
-                    mr={2}
+                    ml={2}
                   >
-                    Forgot password?
+                    SIGN UP
                   </Link>
                 </Flex>
-              </Box>
-              <Button
-                mt={2}
-                colorScheme="green"
-                isLoading={props.isSubmitting}
-                type="submit"
-              >
-                Login
-              </Button>
-            </Form>
+                <Divider my={2} />
+                <InputField
+                  name="usernameOrEmail"
+                  placeholder="Email or username"
+                  label="Email or username"
+                />
+                <Box mt={4}>
+                  <InputField
+                    name="password"
+                    placeholder="password"
+                    label="Password"
+                    type="Password"
+                  />
+                  <Flex>
+                    <Link
+                      href="/forgot-password"
+                      as={NextLink}
+                      color="blue"
+                      fontSize={"0.75rem"}
+                      ml={"auto"}
+                      mr={2}
+                    >
+                      Forgot password?
+                    </Link>
+                  </Flex>
+                </Box>
+                <Button
+                  mt={2}
+                  colorScheme="green"
+                  isLoading={props.isSubmitting}
+                  type="submit"
+                >
+                  Login
+                </Button>
+              </Form>
+            </Card>
           )}
         </Formik>
       </Wrapper>
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { Form, Formik } from "formik";
-import { Button, Box } from "@chakra-ui/react";
+import {
+  Button,
+  Box,
+  Heading,
+  Link,
+  Text,
+  Flex,
+  Card,
+  Divider,
+  Center,
+} from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import InputField from "../components/InputField";
 import { useRegisterMutation } from "../graphql/generated/graphql";
@@ -10,6 +20,7 @@ import { NavBar } from "../components/NavBar";
 import { Container } from "../components/Container";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import NextLink from "next/link";
 
 interface registerProps {}
 
@@ -33,32 +44,55 @@ const Register: React.FC<registerProps> = ({}) => {
           }}
         >
           {(props) => (
-            <Form>
-              <InputField
-                name="username"
-                placeholder="username"
-                label="Username"
-              />
-              <Box mt={4}>
-                <InputField name="email" placeholder="email" label="Email" />
-              </Box>
-              <Box mt={4}>
+            <Card
+              p={8}
+              boxShadow="rgba(0, 0, 0, 0.12) 0px 2px 8px 0px, rgba(0, 0, 0, 0.16) 0px 0px 2px 0px"
+            >
+              <Form>
+                <Center>
+                  <Heading>Sign up</Heading>
+                </Center>
+                <Flex my={2} justifyContent="center">
+                  <Text fontSize={"0.75rem"} color="gray">
+                    ALREADY HAVE AN ACCOUNT?
+                  </Text>
+                  <Link
+                    href="/login"
+                    as={NextLink}
+                    color="blue"
+                    fontSize={"0.75rem"}
+                    ml={2}
+                  >
+                    LOG IN
+                  </Link>
+                </Flex>
+                <Divider my={2} />
                 <InputField
-                  name="password"
-                  placeholder="password"
-                  label="Password"
-                  type="password"
+                  name="username"
+                  placeholder="username"
+                  label="Username"
                 />
-              </Box>
-              <Button
-                mt={6}
-                colorScheme="green"
-                isLoading={props.isSubmitting}
-                type="submit"
-              >
-                Register
-              </Button>
-            </Form>
+                <Box mt={4}>
+                  <InputField name="email" placeholder="email" label="Email" />
+                </Box>
+                <Box mt={4}>
+                  <InputField
+                    name="password"
+                    placeholder="password"
+                    label="Password"
+                    type="password"
+                  />
+                </Box>
+                <Button
+                  mt={6}
+                  colorScheme="green"
+                  isLoading={props.isSubmitting}
+                  type="submit"
+                >
+                  Register
+                </Button>
+              </Form>
+            </Card>
           )}
         </Formik>
       </Wrapper>
