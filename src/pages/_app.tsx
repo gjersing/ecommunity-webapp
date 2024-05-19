f24b0abcdb40d21caa69d2df306438321ff6b3a3
gjersing <csgjersing@gmail.com>
2024-05-18 17:47:33 -0700

    Install next-apollo and use to create withApollo wrapper for true/false SSR by page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,59 +3,26 @@ import theme from "../theme";
 import { AppProps } from "next/app";
 import { IsClientContextProvider } from "../utils/isClientContext";
 import { IsMobileContextProvider } from "../utils/isMobileContext";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import Head from "next/head";
-import { PaginatedPosts } from "../graphql/generated/graphql";
-
-const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_API_URL as string,
-  credentials: "include",
-  // headers: cookie ? { cookie } : undefined,
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          posts: {
-            keyArgs: [],
-            merge(
-              existing: PaginatedPosts | undefined,
-              incoming: PaginatedPosts
-            ): PaginatedPosts {
-              return {
-                ...incoming,
-                posts: [...(existing?.posts || []), ...incoming?.posts],
-              };
-            },
-          },
-        },
-      },
-    },
-  }),
-});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <IsClientContextProvider>
-        <IsMobileContextProvider>
-          <ChakraProvider theme={theme}>
-            <Head>
-              <title>ECOmmunity</title>
-              <meta
-                name="description"
-                content="Social media for nature lovers"
-              />
-              <link
-                rel="shortcut icon"
-                href="https://cdn-icons-png.flaticon.com/512/6041/6041531.png"
-                type="image/x-icon"
-              />
-            </Head>
-            <Component {...pageProps} />
-          </ChakraProvider>
-        </IsMobileContextProvider>
-      </IsClientContextProvider>
-    </ApolloProvider>
+    <IsClientContextProvider>
+      <IsMobileContextProvider>
+        <ChakraProvider theme={theme}>
+          <Head>
+            <title>ECOmmunity</title>
+            <meta name="description" content="Social media for nature lovers" />
+            <link
+              rel="shortcut icon"
+              href="https://cdn-icons-png.flaticon.com/512/6041/6041531.png"
+              type="image/x-icon"
+            />
+          </Head>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </IsMobileContextProvider>
+    </IsClientContextProvider>
   );
 }
 
---
+++ src/utils/withApollo.ts
@@ -0,0 +1,31 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { withApollo as createWithApollo } from "next-apollo";
+import { PaginatedPosts } from "../graphql/generated/graphql";
+
+const client = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_API_URL as string,
+  credentials: "include",
+  // headers: cookie ? { cookie } : undefined,
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          posts: {
+            keyArgs: [],
+            merge(
+              existing: PaginatedPosts | undefined,
+              incoming: PaginatedPosts
+            ): PaginatedPosts {
+              return {
+                ...incoming,
+                posts: [...(existing?.posts || []), ...incoming?.posts],
+              };
+            },
+          },
+        },
+      },
+    },
+  }),
+});
+
+export const withApollo = createWithApollo(client);
