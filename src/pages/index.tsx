01e91fb9dda265a8e4e2c9835ee852f56edb009c
gjersing <csgjersing@gmail.com>
2024-05-23 11:05:23 -0700

    Utilize React Infinite Scroller over scroll eventListeners

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,8 @@ import { NavBar } from "../components/NavBar";
 import { usePostsQuery } from "../graphql/generated/graphql";
 import { VStack, Spinner } from "@chakra-ui/react";
 import { PostCard } from "../components/PostCard";
-import { useEffect } from "react";
 import { withApollo } from "../utils/withApollo";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Index = () => {
   const { data, error, loading, fetchMore, variables } = usePostsQuery({
@@ -12,7 +12,7 @@ const Index = () => {
       limit: 4,
       cursor: null,
     },
-    // notifyOnNetworkStatusChange: true,
+    notifyOnNetworkStatusChange: true,
   });
 
   // TO DO: Remove for v1.0
@@ -20,45 +20,36 @@ const Index = () => {
     return <div>Something went wrong. Error: {error.message}</div>;
   }
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const { scrollTop, clientHeight, scrollHeight } =
-        document.documentElement;
-      if (
-        scrollTop + clientHeight >= scrollHeight - 20 &&
-        data?.posts.hasMore &&
-        !loading
-      ) {
-        fetchMore({
-          variables: {
-            limit: variables?.limit,
-            cursor: data
-              ? data.posts.posts[data.posts.posts.length - 1].createdAt
-              : null,
-          },
-        });
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [variables, data]);
+  const fetchData = () => {
+    fetchMore({
+      variables: {
+        limit: variables?.limit,
+        cursor: data
+          ? data.posts.posts[data.posts.posts.length - 1].createdAt
+          : null,
+      },
+    });
+  };
 
   return (
     <Container height="100%">
       <NavBar />
-      <VStack my={8} spacing={8} maxW={["100vw", "90vw", "80vw", "600px"]}>
-        {!data ? (
-          <Spinner />
-        ) : (
-          data.posts.posts.map((p) =>
-            !p ? null : <PostCard post={p} key={p.id} />
-          )
-        )}
-        {loading && data?.posts.hasMore ? <Spinner /> : null}
-      </VStack>
+      <InfiniteScroll
+        dataLength={data?.posts.posts.length || 0}
+        next={fetchData}
+        hasMore={data?.posts.hasMore || false}
+        loader={<Spinner />}
+      >
+        <VStack my={8} spacing={8} maxW={["100vw", "90vw", "80vw", "600px"]}>
+          {!data ? (
+            <Spinner />
+          ) : (
+            data.posts.posts.map((p) =>
+              !p ? null : <PostCard post={p} key={p.id} />
+            )
+          )}
+        </VStack>
+      </InfiniteScroll>
     </Container>
   );
 };
